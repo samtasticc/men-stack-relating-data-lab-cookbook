44f0b4cc615afc85575545f559713584c0a0a482
samtasticc <samantha.henesy@gmail.com>
2024-10-07 21:19:48 -0400

    create foodSchema and embed inside userSchema

--- models/user.js
+++ models/user.js
@@ -1,5 +1,22 @@
 const mongoose = require('mongoose');
 
+const foodSchema = new mongoose.Schema({
+  food: {
+    type: String,
+    required: true,
+  },
+  perishable: {
+    type: String,
+    enum: ['Yes', 'No'],
+    required: true,
+  },
+  category: {
+    type: String,
+    enum: ['Bakery', 'Beverages', 'Dairy', 'Deli', 'Pantry', 'Produce', 'Protein', 'Snacks'],
+    required: true,
+  }
+})
+
 const userSchema = mongoose.Schema({
   username: {
     type: String,
@@ -9,6 +26,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  pantry: [foodSchema],
 });
 
 const User = mongoose.model('User', userSchema);
