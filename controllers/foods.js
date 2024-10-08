ceef12f807b2d655f48b9d696888ff12587807d7
samtasticc <samantha.henesy@gmail.com>
2024-10-08 19:52:10 -0400

    define create route

--- controllers/foods.js
+++ controllers/foods.js
@@ -19,6 +19,19 @@ router.get('/new', (req, res) => {
     res.render('foods/new.ejs')
 })
 
+// This route will create new foods in the embedded pantry array on the user model.
+router.post('/', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id) // Look up the user from req.session
+        currentUser.foods.push(req.body) // Push req.body (the new form data object) to the pantry array of the current user.
+        await currentUser.save() // Save changes to the user.
+        res.redirect(`users/${currentUser._id}/wines`) // Redirect back to the application’s index view.
+    } catch (error){
+        console.log(error)
+        res.redirect('/') // If any errors, log them and redirect back home /.
+    }
+})
+
 router.delete('/:pantryId', async (req, res) => {
     try {
         const currentUser = await User.findById(req.session.user._id)
